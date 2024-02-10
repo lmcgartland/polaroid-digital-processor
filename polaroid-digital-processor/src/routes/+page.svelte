de74ebf4ef6025598a0ee70ddc8e56a1437b7ed9
lmcgartland <luke.mcgartland@me.com>
2024-02-09 17:55:16 -0800

    Locate multiple polaroids

--- polaroid-digital-processor/src/routes/+page.svelte
+++ polaroid-digital-processor/src/routes/+page.svelte
@@ -7,6 +7,11 @@
 	let image: string | ArrayBuffer | null;
 	let srcImage: string | ArrayBuffer | null;
 
+	// Configure the matrix of how polaroids are arranged in image
+	let polaroidsWide = 2;
+
+	let polaroidDetectionWidth = 500;
+
 	onMount(() => {
 		if (browser && 'cv' in window) {
 			isReady = true;
@@ -18,29 +23,137 @@
 	});
 
 	function processImage() {
+		// Assumes using 600px polaroid
+
 		// Opencv reads from the DOM element with the id "preview"
 		let cv = window.cv;
-		// window.cv.imread('inputImage');
 		let src = cv.imread('inputImage');
 		let dst = new cv.Mat();
 		let gray = new cv.Mat();
 		let opening = new cv.Mat();
-		let coinsBg = new cv.Mat();
-		cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-		cv.threshold(gray, gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU);
+		let polaroidsBg = new cv.Mat();
+		let polaroidsFg = new cv.Mat();
+		let distTrans = new cv.Mat();
+		let unknown = new cv.Mat();
+		let markers = new cv.Mat();
 
-		// get background
-		let M = cv.Mat.ones(3, 3, cv.CV_8U);
+		// Image pre-processing
+		// Resize the image based on how many polaroids are in the image
+		let newWidth = polaroidDetectionWidth * polaroidsWide;
+		let newHeight = (newWidth / src.cols) * src.rows;
+		cv.resize(src, src, new cv.Size(newWidth, newHeight), 0, 0, cv.INTER_AREA);
+		// Blur the image
+		cv.medianBlur(src, src, 7);
+
+		// Convert to grayscale
+		cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+
+		// Thresholding to isolate Polaroids
+		cv.threshold(gray, gray, 180, 255, cv.THRESH_BINARY);
+
+		// Erosion and Dilation to remove noise and get background
+		let M = cv.Mat.ones(12, 12, cv.CV_8U); // Increase the size of the structuring element
 		cv.erode(gray, gray, M);
 		cv.dilate(gray, opening, M);
-		cv.dilate(opening, coinsBg, M, new cv.Point(-1, -1), 3);
 
-		cv.imshow('canvasOutput', coinsBg);
+		// Distance Transform
+		cv.distanceTransform(opening, distTrans, cv.DIST_L2, 5);
+		cv.normalize(distTrans, distTrans, 1, 0, cv.NORM_INF);
+
+		// Get foreground
+		cv.threshold(distTrans, polaroidsFg, 0.7 * 1, 255, cv.THRESH_BINARY);
+		polaroidsFg.convertTo(polaroidsFg, cv.CV_8U, 1, 0);
+		cv.subtract(opening, polaroidsFg, unknown); // Use 'opening' instead of 'polaroidsBg'
+
+		// Get connected components markers
+		cv.connectedComponents(polaroidsFg, markers);
+		for (let i = 0; i < markers.rows; i++) {
+			for (let j = 0; j < markers.cols; j++) {
+				markers.intPtr(i, j)[0] = markers.ucharPtr(i, j)[0] + 1;
+				if (unknown.ucharPtr(i, j)[0] == 255) {
+					markers.intPtr(i, j)[0] = 0;
+				}
+			}
+		}
+
+		// Watershed Algorithm
+		cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
+		cv.watershed(src, markers);
+
+		// Draw barriers
+		for (let i = 0; i < markers.rows; i++) {
+			for (let j = 0; j < markers.cols; j++) {
+				if (markers.intPtr(i, j)[0] == -1) {
+					src.ucharPtr(i, j)[0] = 255; // R
+					src.ucharPtr(i, j)[1] = 0; // G
+					src.ucharPtr(i, j)[2] = 0; // B
+				}
+			}
+		}
+
+		// Display the result
+		cv.imshow('canvasOutput', src);
+
+		// Find the rectangles in the barriers
+		let contours = new cv.MatVector();
+		let hierarchy = new cv.Mat();
+		cv.findContours(markers, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+
+		// Loop through the contours and find the bounding rectangles
+		for (let i = 0; i < contours.size(); ++i) {
+			let cnt = contours.get(i);
+			let rect = cv.boundingRect(cnt);
+
+			// Discard small rectangles
+			if (rect.width < 100 || rect.height < 100) {
+				continue;
+			}
+			// Discard large rectangles
+			if (rect.width > 800 || rect.height > 800) {
+				continue;
+			}
+
+			let x = rect.x;
+			let y = rect.y;
+			let w = rect.width;
+			let h = rect.height;
+			cv.rectangle(src, new cv.Point(x, y), new cv.Point(x + w, y + h), [0, 255, 0, 255], 2);
+
+			// Draw the rectangles
+			cv.imshow('canvasOutput', src);
+		}
+
+		// let cnt = contours.get(0);
+		// let rect = cv.boundingRect(cnt);
+		// let x = rect.x;
+		// let y = rect.y;
+		// let w = rect.width;
+		// let h = rect.height;
+
+		// // Crop the image
+		// let cropped = src.roi(new cv.Rect(x, y, w, h));
+		// cv.imshow('canvasOutput', cropped);
+
+		// // Extract the cropped image from the original, unblurred image
+		// let img = document.getElementById('preview') as HTMLImageElement;
+		// let canvas = document.createElement('canvas');
+		// canvas.width = w;
+		// canvas.height = h;
+		// let ctx = canvas.getContext('2d');
+		// ctx?.drawImage(img, x, y, w, h, 0, 0, w, h);
+		// let dataURL = canvas.toDataURL('image/png');
+		// image = dataURL;
+
+		// Memory cleanup
 		src.delete();
 		dst.delete();
 		gray.delete();
 		opening.delete();
-		coinsBg.delete();
+		polaroidsBg.delete();
+		polaroidsFg.delete();
+		distTrans.delete();
+		unknown.delete();
+		markers.delete();
 		M.delete();
 	}
 
